bc53c91277c820273aca4562c7f86880907adc67
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-27 23:41:59 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -12,7 +14,8 @@ def crew
 end
 
 def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+  # crew #=> ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+  crew.map {|crew_member| "Hello #{crew_member}."}
 end
 
 def engage
--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -1,8 +1,10 @@
 
+## If truly looking for accuracy, please note Leonaro leads and Donatello does machines...
+
 def turtles
     [
       {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
-      {name: "Michaelangelo", weapon: "nunchuks", traits: "party dude"},
+      {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"]},
       {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
       {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
     ]
@@ -11,6 +13,7 @@ end
 def turtle_traits(turtles)
   turtles.map do |turtle|
     turtle[:traits].each do |trait|
+      # trait #=> "serious"
       trait
     end
   end
